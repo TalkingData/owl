7fc230fece2d0100f993084c60ea94bbba68bcf5
吴迎松 <yingsong.wu@tendcloud.com>
2017-09-12 15:58:17 +0800

    修改kafka异步写为同步写

--- repeater/backend/kafka.go
+++ repeater/backend/kafka.go
@@ -7,12 +7,12 @@ import (
 )
 
 type KafkaBackend struct {
-	Producer sarama.AsyncProducer
+	Producer sarama.SyncProducer
 	Topic    string
 }
 
 func NewKafkaBackend(brokers []string, topic string) (*KafkaBackend, error) {
-	producer, err := sarama.NewAsyncProducer(brokers, nil)
+	producer, err := sarama.NewSyncProducer(brokers, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -26,13 +26,8 @@ func NewKafkaBackend(brokers []string, topic string) (*KafkaBackend, error) {
 func (backend *KafkaBackend) Write(data *types.TimeSeriesData) error {
 	message := &sarama.ProducerMessage{
 		Topic: backend.Topic,
-		Key:   nil,
 		Value: sarama.StringEncoder(string(data.Encode())),
 	}
-	select {
-	case backend.Producer.Input() <- message:
-		return nil
-	case err := <-backend.Producer.Errors():
-		return err
-	}
+	_, _, err := backend.Producer.SendMessage(message)
+	return err
 }
